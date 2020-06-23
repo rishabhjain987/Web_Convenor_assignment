5f133a8bbb4bba0c4630a03d2c7f35b0da52da87
rishabhjain987 <64086359+rishabhjain987@users.noreply.github.com>
2020-06-23 23:50:41 +0530

    Add files via upload

---
+++ ITC_Assignment/ITSP_BACK/ITSP/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import itsp_team, itsp_team_name
+admin.site.site_header = "ITSP Admin"
+admin.site.site_title = "ITSP Admin Area"
+admin.site.index_title = "Welcome to ITSP Admin Area"
+
+admin.site.register(itsp_team)
+admin.site.register(itsp_team_name)
