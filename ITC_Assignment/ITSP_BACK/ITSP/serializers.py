b24dfebad0daf3531a0d2daa13210c1ab80cce9e
rishabhjain987 <64086359+rishabhjain987@users.noreply.github.com>
2020-06-24 12:21:38 +0530

    Add files via upload

--- ITC_Assignment/ITSP_BACK/ITSP/serializers.py
+++ ITC_Assignment/ITSP_BACK/ITSP/serializers.py
@@ -2,5 +2,6 @@ from rest_framework import serializers
 from .models import itsp_team
 
 class itsp_teamSerializer(serializers.ModelSerializer):
-	model = itsp_team
-	fields = '__all__'
+	class Meta:
+		model = itsp_team
+		fields = '__all__'
