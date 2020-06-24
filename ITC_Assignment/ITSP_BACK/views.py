d767b673c562e062e32da6b1f0ef2ed8a257db94
rishabhjain987 <64086359+rishabhjain987@users.noreply.github.com>
2020-06-24 11:58:34 +0530

    Delete views.py

--- ITC_Assignment/ITSP_BACK/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def login_form(request):
-	return render(request, 'login_page.html')
-
-def submit_form(request):
-	return HttpResponse("Welcome")
