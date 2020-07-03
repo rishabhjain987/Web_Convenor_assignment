4d7366dcee4f33b93d525684f30138e5a02a7ca8
Rishabh Jain <64086359+rishabhjain987@users.noreply.github.com>
2020-07-04 00:21:09 +0530

    Add files via upload

--- ITC_Assignment/ITSP_BACK/ITSP/views.py
+++ ITC_Assignment/ITSP_BACK/ITSP/views.py
@@ -5,28 +5,33 @@ from .serializers import itsp_teamSerializer
 # Create your views here.
 from .models import itsp_team
 
-def itsp_home(request):
 
-	queryset = itsp_team.objects.all()
-	context = {
-			'itsp_teamname' : queryset
-			}
-	return render(request, 'ITSP/itsp_home_page.html',context)
+def itsp_home(request):
+    queryset = itsp_team.objects.all()
+    context = {
+        'itsp_teamname': queryset
+    }
+    return render(request, 'ITSP/itsp_home_page.html', context)
+
 
 def dynamic_view(request, my_id):
-	obj = get_object_or_404(itsp_team, id=my_id)
-	context = {'object': obj }
-	return render(request, "ITSP/team_details.html", context)
+    # obj = get_object_or_404(itsp_team, id=my_id)
+    obj = itsp_team.objects.filter(Team_ID=my_id)
+    context = {
+        'object': obj
+    }
+    return render(request, "ITSP/team_details.html", context)
+
 
 def landing_page(request):
-	return render(request, "index.html")
+    return render(request, "index.html")
+
 
 class itsp_team_API(APIView):
-	def get(self, request):
-		itsp_team_r = itsp_team.objects.all()
-		serializer = itsp_teamSerializer(itsp_team_r, many = True)
-		return Response(serializer.data)
+    def get(self, request):
+        itsp_team_r = itsp_team.objects.all()
+        serializer = itsp_teamSerializer(itsp_team_r, many=True)
+        return Response(serializer.data)
 
-
-	def post(self):
-		pass
+    def post(self):
+        pass
